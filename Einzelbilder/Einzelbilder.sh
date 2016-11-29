55e617b537672e91a76cb7f5750f2a35d0aa9fa3
TottiToad2016 <torsten@rootsway.de>
2016-11-29 21:53:40 +0100

    Update Einzelbilder.sh

--- Einzelbilder/Einzelbilder.sh
+++ Einzelbilder/Einzelbilder.sh
@@ -4,4 +4,4 @@ eog -f -w /home/pi/Fotokiste/blackscreen.png & #Balckscreen wird über Terminal 
 
 mkdir -p -m 777 /home/pi/Fotokiste/Bilder/"`date --iso-8601`" && cd $_ #erstellt ein Verzeichniss des heutigen Tages im Ordner Unterordner Bilder und wechselt in diesen Ornder
 
-gphoto2 --wait-event-and-download --hook-script=/home/pi/Fotokiste/Fotobox_hook.sh --filename="Fotobox-%Y-%m-%d--%H-%M-%S.%C" --force-overwrite #Foto wird gemacht angezeigt und runtergeladen
+gphoto2 --capture-image --hook-script=/home/pi/Fotokiste/Fotobox_hook.sh --filename="Fotobox-%Y-%m-%d--%H-%M-%S.%C" --force-overwrite #Foto wird gemacht angezeigt und runtergeladen
