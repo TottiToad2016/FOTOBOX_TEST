ae0447d52b766b028f002a034c716fd2a0b2fb30
TottiToad2016 <torsten@rootsway.de>
2016-11-29 17:17:43 +0100

    Create test-hook.sh

---
+++ Test/test-hook.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+
+self=`basename $0`
+
+case "$ACTION" in
+   init)
+echo "$self: INIT"
+# exit 1 # non-null exit to make gphoto2 call fail
+;;
+   start)
+echo "$self: START"
+;;
+   download)
+echo "$self: DOWNLOAD to $ARGUMENT"
+./startSlideshow.sh "$ARGUMENT"
+;;
+   stop)
+echo "$self: STOP"
+;;
+   *)
+echo "$self: Unknown action: $ACTION"
+;;
+esac
+
+exit 0
