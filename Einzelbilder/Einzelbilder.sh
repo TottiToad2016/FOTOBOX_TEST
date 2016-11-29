3394adce06bdbeeccbaa7b0081521be2162988ab
TottiToad2016 <torsten@rootsway.de>
2016-11-29 19:10:08 +0100

    Update Einzelbilder.sh

--- Einzelbilder/Einzelbilder.sh
+++ Einzelbilder/Einzelbilder.sh
@@ -2,4 +2,4 @@
 
 mkdir -p -m 777 /home/pi/Fotokiste/Bilder/"`date --iso-8601`" && cd $_ #erstellt ein Verzeichniss des heutigen Tages im Ordner Unterordner Bilder und wechselt in diesen Ornder
 
-gphoto2 --wait-event-and-download --hook-script=/home/pi/Fotokiste/Fotobox_hook.sh --filename="Fotobox-%Y-%m-%d--%H-%M.%C" --force-overwrite #Foto wird gemacht angezeigt und runtergeladen
+gphoto2 --wait-event-and-download --hook-script=/home/pi/Fotokiste/Fotobox_hook.sh --filename="Fotobox-%Y-%m-%d--%H-%M-%S.%C" --force-overwrite #Foto wird gemacht angezeigt und runtergeladen
