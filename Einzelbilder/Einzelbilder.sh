5654bafd2a4bea08c3d7d23803e65e861e417276
TottiToad2016 <torsten@rootsway.de>
2016-11-29 15:38:47 +0100

    Update Einzelbilder.sh

--- Einzelbilder/Einzelbilder.sh
+++ Einzelbilder/Einzelbilder.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /fotobox2/Bilder/
+cd fotobox2/Bilder/
 mkdir "`date --iso-8601`" && cd $_ #erstellt ein Verzeichniss des heutigen Tages und wechselt in diesen Ornder
 
 gphoto2 --capture-and-download --hook-script=Fotobox_hook.sh --filename="Fotobox-%Y-%m-%d--%H-%M.%C" --force-overwrite #Foto wird gemacht angezeigt und runtergeladen
