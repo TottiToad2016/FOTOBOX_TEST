6c4558981baa15a5a9dace6f84aced65d62dfe63
TottiToad2016 <torsten@rootsway.de>
2016-12-17 15:55:09 +0100

    Update 4er_collage.py

--- Test6/4er_collage.py
+++ Test6/4er_collage.py
@@ -26,14 +26,17 @@ pygame.mouse.set_visible(0) # Mousezeiger unsichtbar ?
 GPIO.setmode(GPIO.BCM)
 SWITCH = 24
 GPIO.setup(SWITCH, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-RESET = 25
-GPIO.setup(RESET, GPIO.IN)
+SHUTDOWN = 25
+GPIO.setup(SHUTDOWN, GPIO.IN)
 PRINT_LED = 22
 POSE_LED = 18
 BUTTON_LED = 23
+ON_LED = 15
 GPIO.setup(POSE_LED, GPIO.OUT)
 GPIO.setup(BUTTON_LED, GPIO.OUT)
 GPIO.setup(PRINT_LED, GPIO.OUT)
+GPIO.setup(ON_LED, GPIO.OUT)
+GPIO.setup(ON_LED, True)
 GPIO.output(BUTTON_LED, True)
 GPIO.output(PRINT_LED, False)
 
@@ -65,7 +68,6 @@ idle_slideshow = True
 # Display time of pictures in the slideshow
 slideshow_display_time = 5
 
-
 ########################
 ### Class definieren ###
 ########################
@@ -76,7 +78,7 @@ class Photobooth:
     """
 
     def __init__(self, display_size, picture_basename, picture_size, pose_time, display_time,
-                 trigger_channel, shutdown_channel, lamp_channel, idle_slideshow, slideshow_display_time):
+                 SWITCH, SHUTDOWN, POSE_LED, ON_LED, idle_slideshow, slideshow_display_time):
         self.display      = GuiModule('Photobooth', display_size)
         self.pictures     = PictureList(picture_basename)
         self.camera       = CameraModule(picture_size)
@@ -85,9 +87,10 @@ class Photobooth:
         self.pose_time    = pose_time
         self.display_time = display_time
 
-        self.trigger_channel  = trigger_channel
-        self.shutdown_channel = shutdown_channel
-        self.lamp_channel     = lamp_channel
+      #  self.SWITCH       = SWITCH
+      #  self.SHUTDOWN     = SHUTDWON
+      #  self.POSE_LED     = POSE_LED
+      #  self.ON_LED       = ON_LED
 
         self.idle_slideshow = idle_slideshow
         if self.idle_slideshow:
@@ -95,6 +98,17 @@ class Photobooth:
             self.slideshow = Slideshow(display_size, display_time, 
                                        os.path.dirname(os.path.realpath(picture_basename)))
 
-        input_channels    = [ trigger_channel, shutdown_channel ]
-        output_channels   = [ lamp_channel ]
-        self.gpio         = GPIO(self.handle_gpio, input_channels, output_channels)
+        #input_channels    = [ trigger_channel, shutdown_channel ]
+        #output_channels   = [ lamp_channel ]
+        #self.gpio         = GPIO(self.handle_gpio, input_channels, output_channels)
+        
+    def teardown(self):
+            self.display.clear()
+            self.display.show_message("SHUTDOWN !!!")
+            self.display.apply()
+            GPIO.output(ON_LED, 0)
+            sleep(0.5)
+            self.display.teardown()
+            #self.gpio.teardown()
+            exit(0)
+     
